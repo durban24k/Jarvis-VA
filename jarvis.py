7d6d2ff138925119f9f3c86dbecc314a9985c5a5
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2020-10-30 22:52:33 +0300

    commands

--- jarvis.py
+++ jarvis.py
@@ -1,9 +1,28 @@
 import JarvisAI
+import re
+import pprint
+import random
 
 obj=JarvisAI.JarvisAssistant()
 
-# def t2s(text):
-#      obj.text2speech(text)
+def t2s(text):
+     obj.text2speech(text)
 
 while True:
-     response=obj.mic_input()
+     res=obj.mic_input()
+
+     if re.search('what can you do', res):
+          li_commands = {
+               "open websites": "Example: 'open youtube.com",
+               "time": "Example: 'what time it is?'",
+               "date": "Example: 'what date it is?'",
+               "launch applications": "Example: 'launch chrome'",
+               "tell me": "Example: 'tell me about India'",
+               "weather": "Example: 'what weather/temperature in Mumbai?'",
+               "news": "Example: 'news for today' ",
+          }
+          ans = """I can do lots of things, for example you can ask me time, date, weather in your city,
+          I can open websites for you, launch application and more. See the list of commands-"""
+          print(ans)
+          pprint.pprint(li_commands)
+          t2s(ans)
